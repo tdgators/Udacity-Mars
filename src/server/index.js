3b0fa9c575487c2c8182c0b3744f4d69563361b2
Tom <tvdonnelly@gmail.com>
2022-01-14 14:33:02 +0000

    first commit

---
+++ src/server/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const fetch = require('node-fetch')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('/', express.static(path.join(__dirname, '../public')))
+
+// your API calls
+
+// example API call
+console.log(`${process.env.API_KEY}`)
+app.get('/apod', async (req, res) => {
+    try {
+        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send({ image })
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+// needed to specify ipv4, so added "0.0.0.0",
+app.listen(port, "0.0.0.0", () => console.log(`Example app listening on port ${port}!`))
